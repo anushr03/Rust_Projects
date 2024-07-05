dde2207b292fbc3b4b61352424b789251341814e
Anush Rathod <anush.rathore382@gmail.com>
2024-07-05 17:58:56 -0400

    new features

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,10 +1,23 @@
-use std::io;
+
+
+//understanding difference between mutable and immutable variables
 fn main() {
     let mut x = 5;
     println!("The value of x is {x}");
     x = 6;
     println!("The value of x is {x}");
 
+    shadowing();
 
+}
+// example to showcase shadowing
+fn shadowing() {
+    let x = 5;
+    let x = x + 1;
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+    println!("The value of x is {x}");
 
 }
