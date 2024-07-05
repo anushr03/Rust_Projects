d8acd9409e1d4e3d451ea26cfb3359d8c01a79dd
Anush Rathod <anush.rathore382@gmail.com>
2024-07-05 16:18:11 -0400

    update 1: Final game with Improvement to quit anytime 🎮

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,15 +1,42 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the Number!");
+    println!("Press q to quit any time.");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("Please enter your guess: ");
 
-    let mut guess = String::new();
+    loop{
+        println!("Please enter your guess: ");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("failed to read line");
+        let mut guess = String::new();
 
-    println!("You've guessed: {}",guess);
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line");
+
+        //added functionality to quit game anytime. trim() because &str add newline character     
+        if guess.trim() == "q"{
+            println!("Goodbye;");
+            break;
+        }
+
+        let guess :u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You've guessed: {}",guess);
+
+        match guess.cmp(&secret_number){
+            Ordering::Less=>println!("Too Small!"),
+            Ordering::Greater=> println!("Too Big"),
+            Ordering::Equal=> {
+                println!("You Win");
+                break;
+            }
+        }
+    }
 }
