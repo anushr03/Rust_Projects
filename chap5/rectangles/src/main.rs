1f9997af6bc5edf28ea9667fdc42315cdcc52706
Anush Rathod <anush.rathore382@gmail.com>
2024-07-09 17:33:18 -0400

    rectangle program in three ways

--- chap5/rectangles/src/main.rs
+++ chap5/rectangles/src/main.rs
@@ -36,14 +36,16 @@ fn area(dimension: (u32, u32)) -> u32 {
 //---------------------------------------------------------------
 //Way 3: Using Structs
 
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
 }
 
 fn main() {
+    let scale = 2;
     let rect1 = Rectangle {
-        width: 30,
+        width: dbg!(30*scale),
         height: 50,
     };
 
@@ -51,6 +53,9 @@ fn main() {
         "The area of the rectangle is {} square pixels",
         area(&rect1)
     );
+
+    //println!("rect1 is {:#?}", rect1);
+    dbg!(&rect1);
 
 }
 
