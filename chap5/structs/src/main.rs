1774630d2c565a5ce671d978d9c5dca09830460a
Anush Rathod <anush.rathore382@gmail.com>
2024-07-09 15:59:20 -0400

    structs

--- chap5/structs/src/main.rs
+++ chap5/structs/src/main.rs
@@ -8,12 +8,40 @@ struct User {
 }
 
 fn main() {
+    // Way 1 to using the struct
     let mut user1 = User {
         active: true,
         username: String::from("anushr03"),
         email: String::from("anush.rathore382@gmail.com"),
         sign_in_count: 1,
     };
-
+    println!("\n");
     println!("Current email id: {}", user1.email);
+    user1.email = String::from("anush.rathore@gmail.com");
+    println!("New email id: {}", user1.email);
+
+    let user2 = build_user("andyatwood382@gmail.com".to_string(),"andyatwood".to_string());
+    println!("user2 email id: {}\n", user2.email);
+
+    //Way 3 to using a struct
+    let user3 = User {
+        email: String::from("rocking@gmail.com"),
+        ..user1
+
+    };
+    // In the example above, because we took the values of user1 into user3, 
+    //the ownership of user1's username has gone to user3.
 }
+
+// Way 2 to using a struct
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username,
+        email,
+        sign_in_count: 1
+    }
+}
+
+//tuple structs
+struct Color()
