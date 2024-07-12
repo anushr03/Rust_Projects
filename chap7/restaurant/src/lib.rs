3fe62ae17791320362cbb523b00d52f296989c90
Anush Rathod <anush.rathore382@gmail.com>
2024-07-12 15:09:15 -0400

    chap 07

--- chap7/restaurant/src/lib.rs
+++ chap7/restaurant/src/lib.rs
@@ -1,14 +1,29 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-        fn seat_at_table() {}
+mod front_of_house;
+
+fn deliver_order() {}
+
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        fruit: String,
     }
 
-    mod serving {
-        fn take_order(){}
-        fn serve_order() {}
-        fn take_payment() {}
+    impl Breakfast {
+        pub fn summer(toast: &str) ->Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                fruit: String::from("peaches"),
+            }
+        }
     }
+
+
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
 }
 
 //Paths for Referring to an Item in the Module Tree
@@ -28,4 +43,9 @@ pub fn eat_at_restaurant() {
 
     //relative path
     front_of_house::hosting::add_to_waitlist();
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I would like to have {} toast please", meal.toast);
 }
+
